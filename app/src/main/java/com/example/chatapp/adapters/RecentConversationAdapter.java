4b7a9b45edc3711342515975b99e312635dc41c6
umarfarooq121 <umarfarooq121@users.noreply.github.com>
2023-08-01 01:30:52 +0200

    Version 1.0

---
+++ app/src/main/java/com/example/chatapp/adapters/RecentConversationAdapter.java
@@ -0,0 +1,76 @@
+package com.example.chatapp.adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.databinding.ItemContainerRecentConverBinding;
+import com.example.chatapp.listners.ConversionListener;
+import com.example.chatapp.models.ChatMessage;
+import com.example.chatapp.models.User;
+
+import java.util.List;
+
+public class RecentConversationAdapter extends RecyclerView.Adapter<RecentConversationAdapter.ConversionViewHolder>{
+
+    private final List<ChatMessage> chatMessages;
+    private final ConversionListener conversionListener;
+
+    public RecentConversationAdapter(List<ChatMessage> chatMessages, ConversionListener conversionListener) {
+        this.chatMessages = chatMessages;
+        this.conversionListener = conversionListener;
+    }
+
+    @NonNull
+    @Override
+    public ConversionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ConversionViewHolder(
+                ItemContainerRecentConverBinding.inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        parent,false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecentConversationAdapter.ConversionViewHolder holder, int position) {
+        holder.setData(chatMessages.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    class ConversionViewHolder extends RecyclerView.ViewHolder{
+        ItemContainerRecentConverBinding binding;
+        ConversionViewHolder(ItemContainerRecentConverBinding itemContainerRecentConverBinding){
+            super(itemContainerRecentConverBinding.getRoot());
+            binding = itemContainerRecentConverBinding;
+        }
+
+        void setData(ChatMessage chatMessage){
+            binding.imageprofile.setImageBitmap(getConversionImage(chatMessage.conversionImage));
+            binding.textName.setText(chatMessage.conversionName);
+            binding.textRecentMessage.setText(chatMessage.message);
+            binding.getRoot().setOnClickListener(v -> {
+                User user = new User();
+                user.id = chatMessage.conversionId;
+                user.name= chatMessage.conversionName;
+                user.image =chatMessage.conversionImage;
+                conversionListener.onConversionClicked(user);
+            });
+        }
+    }
+
+    private Bitmap getConversionImage(String encodedImage){
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+    }
+}
